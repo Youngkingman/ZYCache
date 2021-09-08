e011f8a677ec5ec91b2289d1d6822a757320c37e
Youngkingman <879375254@qq.com>
2021-09-08 22:30:39 +0800

    add show & parameter list

--- main.go
+++ main.go
@@ -1,33 +1,30 @@
 package main
 
 import (
-	skiplistwithgo "basic/util"
+	skiplistwithgo "basic/util/skipList"
 	"fmt"
 )
 
 type IntTestKey struct {
+	skiplistwithgo.ParameterListSkey
 	key int
 }
 
 func (key IntTestKey) CompareBiggerThan(other skiplistwithgo.SListKey) bool {
-	return key.key > other.KeyInt()
+	return key.key > other.KeyInt32()
 }
 
-func (key IntTestKey) KeyInt() int {
+func (key IntTestKey) KeyInt32() int {
 	return key.key
-}
-
-func (key IntTestKey) KeyString() (ret string) {
-	return
 }
 
 func main() {
 	sklist := skiplistwithgo.GetSkipList(5)
-	for i := 0; i < 2000; i++ {
-		key := IntTestKey{i}
+	for i := 0; i < 20; i++ {
+		key := IntTestKey{skiplistwithgo.ParameterListSkey{}, i}
 		sklist.InsertElement(key, "fuck you")
 	}
-
-	has, val := sklist.Search(IntTestKey{60})
+	has, val := sklist.Search(IntTestKey{skiplistwithgo.ParameterListSkey{}, 60})
 	fmt.Println(has, val)
+	sklist.Show()
 }
