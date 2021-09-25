7c08c0b99a0442ef47344aae8d4c41704bd7bea1
kiman <879375254@qq.com>
2021-09-25 16:25:17 +0800

    rpc communication function

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module basic
 
 go 1.15
+
+require (
+	github.com/go-enry/go-enry/v2 v2.7.1 // indirect
+	github.com/hhatto/gocloc v0.4.1 // indirect
+	github.com/jessevdk/go-flags v1.5.0 // indirect
+	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665 // indirect
+)
---
+++ zhenCache/rpcdef/coodinator.go
@@ -0,0 +1,49 @@
+package rpcdef
+
+import (
+	store "basic/zhenCache/storeService"
+	"errors"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"time"
+)
+
+type Coordinator struct{}
+
+func (c *Coordinator) Set(args *StoreArgs, reply *StoreReply, expire time.Duration) error {
+	if args.Command != SET {
+		reply.Reply = FAIL
+		return errors.New("WRONG COMMAND")
+	}
+	store.SetValue(args.Key, args.Value, expire)
+	reply.Reply = SUCCESS
+	return nil
+}
+
+func (c *Coordinator) Get(args *StoreArgs, reply *StoreReply) error {
+	if args.Command != GET {
+		reply.Reply = FAIL
+		return errors.New("WRONG COMMAND")
+	}
+	val, err := store.GetValue(args.Key)
+	if err != nil {
+		reply.Reply = FAIL
+		return errors.New("NO KEY")
+	}
+	reply.Reply = SUCCESS
+	reply.Value = val
+	return nil
+}
+
+func (c *Coordinator) server() {
+	rpc.Register(c)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":1234")
+
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
